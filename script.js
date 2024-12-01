20b38e2c9965efb02a2cacc8071e9d667ac5b12b
Aydin06-2010 <aydinabdullahaziz@gmail.com>
2024-12-01 11:46:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const startScreenElem = document.querySelector("[data-start-screen]")
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
 document.addEventListener("touchstart", handleStart, { once: true })
+document.addEventListener("keystart", handleStart, { once: true })
 
 let lastTime
 let speedScale
