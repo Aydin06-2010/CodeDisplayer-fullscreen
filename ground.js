da97475b6eeeb4c54ca004d010e6322b2d5bb15e
Aydin06-2010 <aydinabdullahaziz@gmail.com>
2024-12-06 10:49:25 +0600

    Update ground.js

--- ground.js
+++ ground.js
@@ -10,6 +10,9 @@ const groundElems = document.querySelectorAll("[data-ground]")
 export function setupGround() {
   setCustomProperty(groundElems[0], "--left", 0)
   setCustomProperty(groundElems[1], "--left", 300)
+  document.addEventListener("contextmenu", function(e) {
+    e.preventDefault(); 
+});
 }
 
 export function updateGround(delta, speedScale) {
