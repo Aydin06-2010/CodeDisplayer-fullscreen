2ecfa1ade4ca0b042e1db0d09ae0a6dfd2ed93be
Aydin06-2010 <aydinabdullahaziz@gmail.com>
2025-01-05 22:00:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -48,12 +48,13 @@ function checkLose() {
 }
 
 function isCollision(rect1, rect2) {
-  return(rect1.left < rect2.right &&
+  if(rect1.left < rect2.right &&
     rect1.top < rect2.bottom &&
     rect1.right > rect2.left &&
     rect1.bottom > rect2.top
-  );
+  ){
 console.log("$sudo isCollision() return undefined");
+}
 }
 
 function updateSpeedScale(delta) {
